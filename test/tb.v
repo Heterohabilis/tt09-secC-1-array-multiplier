58c840b3510b43e60c1e3f4775d7bce2ff237909
Heterohabilis <xm2204@nyu.edu>
2024-10-25 15:44:19 -0400

    trial: add instance name

--- test/tb.v
+++ test/tb.v
@@ -28,7 +28,7 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-  tt_um_C_1_4bit_multiplier (
+  tt_um_C_1_4bit_multiplier name1 (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
